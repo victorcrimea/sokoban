7404adcb6610b8362c7e8f09fbb1f4a2020fbf5e
victorcrimea <victor_crimea@mail.ru>
2015-08-19 21:02:47 -0400

    Textures unified to 64x64

--- main.cpp
+++ main.cpp
@@ -2,12 +2,41 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 
-
 using namespace sf;
+
+class Actor {
+private:
+	int x;
+	int y;
+public:
+
+};
+
 int main()
 {
 
-	Window App(VideoMode(800, 600, 32), "SFML Window");
+	Window window(VideoMode(800, 600, 32), "SFML Window");
+
+
+	// Главный цикл приложения
+	while(window.IsOpened())
+	{
+		// Обрабатываем события в цикле
+		Event event;
+
+		while(window.GetEvent(event))
+		{
+			// Кроме обычного способа наше окно будет закрываться по нажатию на Escape
+			if(event.Type == Event::Closed || (event.Type == Event::KeyPressed && event.Key.Code == Key::Escape))
+			window.Close();
+		}
+
+		// Тут будут вызываться функции обновления и отрисовки объектов
+		// Отрисовка
+		window.Display();
+	}
+
+
 
 	return 0;
 }
